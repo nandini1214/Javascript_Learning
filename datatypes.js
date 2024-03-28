c5efe341b3f12ee611a7e36ad6df5e416efa7241
Nandini Lohar <99020171+nandini1214@users.noreply.github.com>
2024-03-28 08:44:33 +0000

    stack and heap

--- datatypes.js
+++ datatypes.js
@@ -1,3 +1,17 @@
 
-let bignumber =  Symbol('123')
-console.log(typeof bignumber)
+// let bignumber =  Symbol('123')
+// console.log(typeof bignumber)
+
+// stack (primitive DATA TYPES) , Heap  (non  primitive Data types)
+
+let email = 'ndera9@gmail.com'
+let another_email = email 
+another_email  = "heyanndniw"
+
+let userOne = {
+    email : "nandanidewra2001@gmail.com",
+    Upi : "nan@ydhakd"
+}
+let UserTwo = userOne
+UserTwo.email = "riti@gmail.com"
+console.log(userOne , UserTwo)
