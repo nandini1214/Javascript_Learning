dff57e1f034b0faff877fde31f3fafec61169564
Nandini Lohar <99020171+nandini1214@users.noreply.github.com>
2025-03-13 08:23:11 +0000

    Learning about date and time

---
+++ Basics/date.js
@@ -0,0 +1,10 @@
+const date = new Date()
+console.log(date);
+console.log(date.toString());
+console.log(date.toDateString());
+console.log(date.toLocaleDateString());
+console.log(date.toISOString());
+console.log(date.toJSON());
+
+
+
