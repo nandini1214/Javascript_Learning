4d873aba53b0c54f71c0ba175ce9222f85baff83
Nandini Lohar <99020171+nandini1214@users.noreply.github.com>
2025-03-13 08:22:25 +0000

    Function file

---
+++ 02Basics/function.js
@@ -0,0 +1,10 @@
+function firstDeclaration(){
+    console.log("Nandini")
+}
+firstDeclaration()
+function secondDeclaration(a,b)
+{ 
+    // a and b here is parameters
+    console.log(a+b)
+}
+secondDeclaration(4,"a")// 4 and a here is arguments
